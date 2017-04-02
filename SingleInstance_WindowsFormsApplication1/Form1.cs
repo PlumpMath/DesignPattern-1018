ab3c7f7500c82cc75f565b22f635acc5e6a1535b
Administrator <Administrator@FG4XKSLRKPVGO14>
2017-04-02 22:26:45 +0800

    设计模式值单例模式

---
+++ SingleInstance_WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+
+
+namespace SingleInstance_WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        FormToolBox ftb;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.IsMdiContainer = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenToolBox();
+        }
+
+        private void OpenToolBox()
+        {
+            FormToolBox.GetInstance().Show();
+        }
+    }
+}
---
+++ SingleInstance_WindowsFormsApplication1/FormToolBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SingleInstance_WindowsFormsApplication1
+{
+    public partial class FormToolBox : Form
+    {
+        private static FormToolBox instance=null;
+
+        private FormToolBox()
+        {
+            InitializeComponent();
+        }
+
+        public static FormToolBox GetInstance()
+        {
+            if (instance == null || instance.IsDisposed)
+            {
+                instance=new FormToolBox();
+                instance.MdiParent = Form1.ActiveForm;
+            }
+            return instance;
+        }
+
+        private void FormToolBox_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
